0b8fc6113e03ed5818052abf46a194de6221f810
Rofael-Alfons <rofa.alfons7@gmail.com>
2023-05-21 18:02:03 +0300

    admin login enhanced

--- controllers/admin.js
+++ controllers/admin.js
@@ -8,13 +8,13 @@ async function login(req, res) {
   if (admin) {
     var valid = bcrypt.compareSync(Password, admin.Password);
     if (valid) {
-      var token = jwt.sign(
-        {
-          adminId: admin._id,
-          displayName: admin.displayName,
-        },
-        process.env.SECRET
-      );
+      // var token = jwt.sign(
+      //   {
+      //     adminId: admin._id,
+      //     displayName: admin.displayName,
+      //   },
+      //   process.env.SECRET
+      // );
       res.status(200).json({ token, admin });
     } else {
       res.status(401).json({ message: "Invalid Password" });
@@ -24,4 +24,4 @@ async function login(req, res) {
   }
 }
 
-module.exports = {login};
+module.exports = { login };
